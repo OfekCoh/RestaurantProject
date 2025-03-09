0dff0e38cbaea26b12951e4a7fe7cee03f3ea0c5
alonengel <alonisrael.engel@gmail.com>
2025-03-09 15:03:33 +0200

    feature add - orders, orders step by step , bug fixes

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/BuyerDetails.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/BuyerDetails.java
@@ -4,53 +4,92 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class BuyerDetails {
-
-    private String creditCard;
     private String name;
     private String address;
     private String phone;
+    private String userId;
+    private String cardNumber;
+    private int cardMonth;
+    private int cardYear;
+    private String cvv;
 
-    // Constructors
+    // Constructor, getters, and setters
     public BuyerDetails() {
     }
 
-    public BuyerDetails(String creditCard, String name, String address, String phone) {
-        this.creditCard = creditCard;
+    public BuyerDetails(String name, String address, String phone, String userId,
+                        String cardNumber, int cardMonth, int cardYear, String cvv) {
         this.name = name;
         this.address = address;
         this.phone = phone;
-    }
-
-    // Getters and setters
-    public String getCreditCard() {
-        return creditCard;
-    }
-
-    public void setCreditCard(String creditCard) {
-        this.creditCard = creditCard;
-    }
-
-    public String getName() {
-        return name;
+        this.userId = userId;
+        this.cardNumber = cardNumber;
+        this.cardMonth = cardMonth;
+        this.cardYear = cardYear;
+        this.cvv = cvv;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
+    public String getName() {
+        return name;
     }
 
     public void setAddress(String address) {
         this.address = address;
     }
 
-    public String getPhone() {
-        return phone;
+    public String getAddress() {
+        return address;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardMonth(int cardMonth) {
+        this.cardMonth = cardMonth;
+    }
+
+    public int getCardMonth() {
+        return cardMonth;
+    }
+
+    public void setCardYear(int cardYear) {
+        this.cardYear = cardYear;
+    }
+
+    public int getCardYear() {
+        return cardYear;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getCvv() {
+        return cvv;
     }
 }
